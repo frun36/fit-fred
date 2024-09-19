70d75cd052a4728ab2529c805bc8100a84327384
VictorPierozak <pierozakwiktor@gmail.com>
2024-09-19 17:09:42 +0200

    Fixed run ctest script

--- test/run-ctest-container.sh
+++ test/run-ctest-container.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-docker build -t fred-server-env -f ENV.Dockerfile .
-docker build --no-cache -t fred-server .
+docker build -t fred-server-env -f ENV.Dockerfile ..
+docker build --no-cache -t fred-server -f Dockerfile ..
 docker run --rm fred-server ctest --verbose --output-on-failure

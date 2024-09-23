73db78953f3c075acda70979d5e4af39b9de49a3
frun36 <franek.urbanski@gmail.com>
2024-09-23 12:02:52 +0200

    Complete rebase

--- test/run-ctest-container.sh
+++ test/run-ctest-container.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-docker build -t cc7-fit -f ../CC7-image/Dockerfile ..
-docker build --no-cache -t fred-server -f Dockerfile .. && docker run --rm fred-server ctest3 --verbose --output-on-failure
+#!bin/bash
+docker build -t fred-server-env -f ENV.Dockerfile ..
+docker build --no-cache -t fred-server -f Dockerfile .. && docker run --rm fred-server ctest --verbose --output-on-failure

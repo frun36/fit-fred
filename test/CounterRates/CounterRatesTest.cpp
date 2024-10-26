2c7f7eb299c523725a439d4ef9f316147a20740a
frun36 <franek.urbanski@gmail.com>
2024-10-26 11:10:38 +0200

    Add test for parseFifoAlfResponse

--- test/CounterRates/CounterRatesTest.cpp
+++ test/CounterRates/CounterRatesTest.cpp
@@ -7,7 +7,21 @@ namespace
 
 TEST(CounterRatesTest, FifoAlfResponse)
 {
-    CounterRates(nullptr, 1, 1);
+    CounterRates cr(nullptr, 2, 8);
+    auto resultFailure = cr.parseFifoAlfResponse("failure\n");
+    EXPECT_TRUE(resultFailure.empty());
+
+    auto resultSingle = cr.parseFifoAlfResponse("success\n0x000123456780badf00d\n0x000123456780badbeef\n");
+    vector<vector<uint32_t>> expectedResultSingle = { { 0x0badf00d, 0x0badbeef } };
+    EXPECT_EQ(resultSingle, expectedResultSingle);
+
+    auto resultMultiple = cr.parseFifoAlfResponse(
+        "success\n"
+        "0x000123456780badf00d\n0x000123456780badbeef\n"
+        "0x000123456780badcafe\n0x00012345678900df00d\n"
+        "0x000123456780badf00d\n0x000123456780badbeef\n");
+    vector<vector<uint32_t>> expectedResultMultiple = { { 0x0badf00d, 0x0badbeef }, { 0x0badcafe, 0x900df00d }, { 0x0badf00d, 0x0badbeef } };
+    EXPECT_EQ(resultMultiple, expectedResultMultiple);
 }
 
 } // namespace

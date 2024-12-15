dc26cea7f41f082c46fd4ba0336799578d35f4f5
frun36 <franek.urbanski@gmail.com>
2024-12-15 11:48:02 +0100

    Basic cli interface for conversion script

--- configuration-parser/src/main.rs
+++ configuration-parser/src/main.rs
@@ -1,17 +1,60 @@
-use std::error::Error;
+use std::{error::Error, fs};
 
-use configuration::Configuration;
 use boards::BoardMap;
+use configuration::Configuration;
+
+use clap::Parser;
 
 mod boards;
 mod configuration;
 mod error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let tcm_map = BoardMap::read_file("tcm.csv")?;
-    let pm_map = BoardMap::read_file("pm.csv")?;
-    let configuration = Configuration::read_file("fv0/phys_18ch_05_221109.cfg")?;
+#[derive(Parser, Debug)]
+#[command(
+    name = "Configuration parser",
+    version = "1.0",
+    about = "Parses .cfg files from Control Server into SQL INSERT statements for the FRED DB"
+)]
+struct Cli {
+    #[arg(
+        short,
+        long,
+        help = "All .cfg files from this directory will be parsed"
+    )]
+    input_dir: String,
 
-    configuration.convert_and_save_sql(&tcm_map, &pm_map, "Varlen Grabski").expect("Couldn't convert or save SQL");
+    #[arg(short, long, help = "output directory (must exist)", default_value_t = String::from("sql"))]
+    output_dir: String,
+
+    #[arg(short, long)]
+    author: String,
+
+    #[arg(long, help = ".csv with description of TCM parameters", default_value_t = String::from("tcm.csv"))]
+    tcm_params: String,
+
+    #[arg(long, help = ".csv with description of PM parameters", default_value_t = String::from("pm.csv"))]
+    pm_params: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    let tcm_map = BoardMap::read_file(&args.tcm_params).expect("Invalid TCM map file");
+    let pm_map = BoardMap::read_file(&args.pm_params).expect("Invalid PM map file");
+
+    for dir_entry in fs::read_dir(args.input_dir)? {
+        let path_buf = dir_entry?.path();
+        let path = path_buf.to_str().ok_or(crate::error::Error::msg(
+            "Configuration file path is not valid UTF-8".to_string(),
+        ))?;
+        let configuration = Configuration::read_file(path)?;
+
+        configuration
+            .convert_and_save_sql(&tcm_map, &pm_map, &args.author)
+            .expect("Couldn't convert or save SQL");
+
+        println!();
+    }
+
     Ok(())
 }

2ddd0f543ac3f78a7655f766bd211638ade1c350
wpierozak <pierozakwiktor@gmail.com>
2025-02-07 13:29:31 +0100

    Removed BlockReadTest service

--- mapi/include/mapifactory.h
+++ mapi/include/mapifactory.h
@@ -12,7 +12,6 @@
 #include "services/SetPhaseDelay.h"
 #include "services/CounterRates.h"
 #include "services/ConfigurationDatabaseBroker.h"
-#include "services/BlockReadTest.h"
 #include <exception>
 #include <stdexcept>
 #include <string>
@@ -35,7 +34,6 @@ class MapiFactory
     std::unique_ptr<ResetErrors> m_resetError;
     std::unique_ptr<SetPhaseDelay> m_setPhaseDelay;
     std::unique_ptr<ConfigurationDatabaseBroker> m_saveConfiguration;
-    std::unique_ptr<BlockReadTest> m_blockReadTest;
     
     void generateObjects();
 

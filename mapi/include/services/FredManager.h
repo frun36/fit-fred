285fe84762bb02af67474223f2227c6b37357fe6
frun36 <franek.urbanski@gmail.com>
2025-03-19 14:40:44 +0100

    Reformat

--- mapi/include/services/FredManager.h
+++ mapi/include/services/FredManager.h
@@ -1,7 +1,7 @@
+#pragma once
 
 #include "Fred/Mapi/mapigroup.h"
-#include "services/configurations/BoardConfigurations.h"
-#include <unordered_map>
+#include "board/Board.h"
 #include <cstring>
 #include <list>
 #include <memory>
@@ -9,8 +9,8 @@
 class FredManager : public Mapigroup
 {
    public:
-    FredManager(std::shared_ptr<Board> TCM, const std::string& configSrv, const std::string& resetSystemSrv, 
-        const std::string& resetErrorsSrv, const std::list<std::string>& looping, const std::list<std::string>& configs);
+    FredManager(std::shared_ptr<Board> TCM, const std::string& configSrv, const std::string& resetSystemSrv,
+                const std::string& resetErrorsSrv, const std::list<std::string>& looping, const std::list<std::string>& configs);
     string processInputMessage(string msg) override;
     string processOutputMessage(string msg) override;
 
@@ -24,11 +24,10 @@ class FredManager : public Mapigroup
     const std::string ReinitializeSpiMaks{ "REINITIALIZE_SPI_MASK" };
 
     const useconds_t DelayAfterReinitializeSpiMask{ 500'000 };
-    const useconds_t DelayAfterConfiguration{250'000};
+    const useconds_t DelayAfterConfiguration{ 250'000 };
 
-    
-    std::vector<std::pair<std::string,std::string>> m_startLooping;
-    std::vector<std::pair<std::string,std::string>> m_stopLooping;
+    std::vector<std::pair<std::string, std::string>> m_startLooping;
+    std::vector<std::pair<std::string, std::string>> m_stopLooping;
     const std::list<std::string> m_configs;
     const std::string m_configurationService;
     const std::string m_resetSystemService;
--- mapi/src/services/FredManager.cpp
+++ mapi/src/services/FredManager.cpp
@@ -1,65 +1,72 @@
 #include "services/FredManager.h"
-#include<unistd.h>
+#include "utils/utils.h"
+#include <sstream>
+#include <unistd.h>
 
-FredManager::FredManager(std::shared_ptr<Board> TCM, const std::string& configSrv, const std::string& resetSystemSrv, const std::string& resetErrorsSrv, const std::list<std::string>& looping, const std::list<std::string>& configs) : m_TCM(TCM),
-m_configurationService(configSrv), m_resetErrorsService(resetErrorsSrv), m_resetSystemService(resetSystemSrv), m_configs(configs)
+FredManager::FredManager(std::shared_ptr<Board> TCM, const std::string& configSrv, const std::string& resetSystemSrv,
+                         const std::string& resetErrorsSrv, const std::list<std::string>& looping, const std::list<std::string>& configs)
+    : m_TCM(TCM),
+      m_configs(configs),
+      m_configurationService(configSrv),
+      m_resetSystemService(resetSystemSrv),
+      m_resetErrorsService(resetErrorsSrv)
 {
-    for(auto& srv: looping){
+    for (auto& srv : looping) {
         m_startLooping.emplace_back(srv, StartService);
         m_stopLooping.emplace_back(srv, StopService);
     }
 }
 
 std::string FredManager::processInputMessage(string msg)
-{   
+{
     std::istringstream stream(msg);
     std::string cmd;
-    std::optional<std::string> config{std::nullopt};
-    string_utils::Splitter split(msg,',');
-    try{
+    std::optional<std::string> config{ std::nullopt };
+    string_utils::Splitter split(msg, ',');
+    try {
         cmd = split.getNext();
-        if(cmd == StartService && !split.reachedEnd()){
+        if (cmd == StartService && !split.reachedEnd()) {
             config = split.getNext();
         }
-    }
-    catch(std::exception& e){
-        publishError(string_utils::concatenate("Failed to parse command: ", msg,"; Exception: ", e.what()));
-        return{};
+    } catch (std::exception& e) {
+        publishError(string_utils::concatenate("Failed to parse command: ", msg, "; Exception: ", e.what()));
+        return {};
     }
 
-    if(cmd == StartService){
+    if (cmd == StartService) {
         startServices(config);
-    } else if(cmd == StopService){
+    } else if (cmd == StopService) {
         stopServices();
     }
     publishAnswer("SUCCESS");
     noRpcRequest = true;
     return {};
 }
+
 void FredManager::startServices(std::optional<std::string> config)
 {
-    newMapiGroupRequest({{m_resetSystemService, ReinitializeSpiMaks}});
+    newMapiGroupRequest({ { m_resetSystemService, ReinitializeSpiMaks } });
     usleep(10'000);
-    while(m_TCM->getEnvironment("RESET_SYSTEM") == 1){
+    while (m_TCM->getEnvironment("RESET_SYSTEM") == 1) {
         usleep(1'000);
     }
-    
-    if(config.has_value()){
-        newMapiGroupRequest({{m_configurationService,config.value()}});
+
+    if (config.has_value()) {
+        newMapiGroupRequest({ { m_configurationService, config.value() } });
         usleep(10'000);
         uint32_t running = 0;
-        while(running != m_configs.size()){
+        while (running != m_configs.size()) {
             running = 0;
-            for(const auto& config: m_configs){
-                if(m_TCM->getEnvironment(config)==0){
+            for (const auto& config : m_configs) {
+                if (m_TCM->getEnvironment(config) == 0) {
                     running += 1;
                 }
             }
             usleep(1'000);
         }
-        newMapiGroupRequest({{m_resetErrorsService,""}});
+        newMapiGroupRequest({ { m_resetErrorsService, "" } });
     }
-    
+
     newMapiGroupRequest(m_startLooping);
 }
 
@@ -68,7 +75,7 @@ void FredManager::stopServices()
     newMapiGroupRequest(m_stopLooping);
 }
 
-std::string FredManager::processOutputMessage(string msg)
+std::string FredManager::processOutputMessage(string)
 {
     return {};
 }
